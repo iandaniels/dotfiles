006331d85e691b4c2644c1afdecb9f040ffb9675
Ian Daniels <ian.daniels@dxc.com>
2021-02-16 09:30:04 +0000

    Updated the PowerShell version

--- Windows/Office-MonthlyChannel.ps1
+++ Windows/Office-MonthlyChannel.ps1
@@ -1,12 +1,20 @@
-rem Sets the Office 365 update channel to monthly
-rem https://docs.microsoft.com/en-us/office365/troubleshoot/administration/switch-channel-for-office-365
+# Sets the Office 365 update channel to monthly
+# https://docs.microsoft.com/en-us/office365/troubleshoot/administration/switch-channel-for-office-365
 
-#setlocal
-#reg query HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Office\ClickToRun\Configuration\ /v CDNBaseUrl
+# if %errorlevel%==0 (goto SwitchChannel) else (goto End)
+# :SwitchChannel
+# reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Office\ClickToRun\Configuration /v CDNBaseUrl /t REG_SZ /d "http://officecdn.microsoft.com/pr/492350f6-3a01-4f97-b9c0-c7c6ddf67d60" /f
+# reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Office\ClickToRun\Configuration /v UpdateUrl /f
+# reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Office\ClickToRun\Configuration /v UpdateToVersion /f
+# reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Office\ClickToRun\Updates /v UpdateToVersion /f
+# reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Office\16.0\Common\OfficeUpdate\ /f
+
 
 $baseUrl = "http://officecdn.microsoft.com/pr/492350f6-3a01-4f97-b9c0-c7c6ddf67d60"
 $regPath = "HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration\"
 $regKey = "CDNBaseUrl"
+$updatesPath = "HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration\"
+$officeUpdatePath = "HKLM:\\SOFTWARE\Policies\Microsoft\Office\16.0\Common\OfficeUpdate\"
 
 if (Test-Path $regPath)
 {
@@ -17,21 +25,30 @@ if (Test-Path $regPath)
         New-ItemProperty -Name $regKey -Path $regPath -PropertyType string -Value $baseUrl -Force
     }
 
-    Remove-ItemProperty -Path $regPath -Value "UpdateUrl" -Force
-    Remove-ItemProperty -Path $regPath -Value "UpdateToVersion" -Force
+    if (Get-ItemProperty -Path $regPath -Name "UpdateUrl" -ErrorAction SilentlyContinue)
+    {
+        Remove-ItemProperty -Path $regPath -Name "UpdateUrl" -Force
+    }
+    
+    if (Get-ItemProperty -Path $regPath -Name "UpdateToVersion" -ErrorAction SilentlyContinue)
+    {
+        Remove-ItemProperty -Path $regPath -Name "UpdateToVersion" -Force
+    }
 }
 
-if (Test-Path HKLM:\\)
+if (Test-Path $updatesPath)
+{
+    if (Get-ItemProperty -Path $updatesPath -Name "UpdateToVersion" -ErrorAction SilentlyContinue)
+    {
+        Remove-ItemProperty -Path $updatesPath -Name "UpdateToVersion" -Force
+    }
+}
 
+if (Test-Path $officeUpdatePath)
+{
+    Remove-Item -Path $officeUpdatePath -Force
+}
 
-if %errorlevel%==0 (goto SwitchChannel) else (goto End)
-:SwitchChannel
-reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Office\ClickToRun\Configuration /v CDNBaseUrl /t REG_SZ /d "http://officecdn.microsoft.com/pr/492350f6-3a01-4f97-b9c0-c7c6ddf67d60" /f
-reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Office\ClickToRun\Configuration /v UpdateUrl /f
-reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Office\ClickToRun\Configuration /v UpdateToVersion /f
-reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Office\ClickToRun\Updates /v UpdateToVersion /f
-reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Office\16.0\Common\OfficeUpdate\ /f
+$updateCommand = join-path $env:CommonProgramFiles '\Microsoft Shared\ClickToRun\OfficeC2RClient.exe' 
 
-"%CommonProgramFiles%\microsoft shared\ClickToRun\OfficeC2RClient.exe" /update user
-:End
-Endlocal 
+& $updateCommand /update user
