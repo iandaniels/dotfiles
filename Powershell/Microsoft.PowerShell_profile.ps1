54db57598055476a55f8c26f4c56ae49c970ab01
Ian Daniels <ian.daniels@dxc.com>
2021-07-16 11:03:33 +0100

    Updated PowerShell profile

--- Powershell/Microsoft.PowerShell_profile.ps1
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,11 @@
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-Theme Paradox
-
 $DefaultUser = 'idaniels'
 
 if ($host.Name -eq 'ConsoleHost')
 {
+    Import-Module posh-git
+    Import-Module oh-my-posh
+    Set-PoshPrompt -Theme Paradox
+
     Import-Module PSReadLine
     Set-PSReadLineOption -EditMode Emacs
 }
