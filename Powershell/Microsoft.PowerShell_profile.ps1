d2318e8876d8d1fd31d766ab679cfabb395d932a
Ian Daniels <ian.daniels@dxc.com>
2021-11-11 10:17:13 +0000

    Updated Oh My Posh settings

--- Powershell/Microsoft.PowerShell_profile.ps1
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,15 @@
 $DefaultUser = 'idaniels'
+$env:POSH_SESSION_DEFAULT_USER = $DefaultUser
 
 if ($host.Name -eq 'ConsoleHost')
 {
     Import-Module posh-git
+    $env:POSH_GIT_ENABLED = $true
     Import-Module oh-my-posh
-    Set-PoshPrompt -Theme Paradox
+    #Set-PoshPrompt -Theme Paradox
+    Set-PoshPrompt -Theme  ~/.oh-my-posh.omp.json
 
     Import-Module PSReadLine
     Set-PSReadLineOption -EditMode Emacs
+    Set-PSReadLineKeyHandler -Chord Ctrl+Alt+f -Function GotoBrace
 }
