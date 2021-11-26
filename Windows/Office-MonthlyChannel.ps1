42b00e6faf83a4a891a1342b88e1e070035e1a06
Ian Daniels <ian.daniels@dxc.com>
2021-11-26 19:37:25 +0000

    Added configuration for Office Current Channel

--- Windows/Office-MonthlyChannel.ps1
+++ Windows/Office-MonthlyChannel.ps1
@@ -9,12 +9,31 @@
 # reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Office\ClickToRun\Updates /v UpdateToVersion /f
 # reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Office\16.0\Common\OfficeUpdate\ /f
 
+# Monthly Enterprise Channel
+# CDNBaseUrl = http://officecdn.microsoft.com/pr/55336b82-a18d-4dd6-b5f6-9e5095c314a6 
+
+# Current Channel 
+# CDNBaseUrl = http://officecdn.microsoft.com/pr/492350f6-3a01-4f97-b9c0-c7c6ddf67d60
+
+# Current Channel (Preview)
+# CDNBaseUrl = http://officecdn.microsoft.com/pr/64256afe-f5d9-4f86-8936-8840a6a4f5be
+
+# Semi-Annual Enterprise Channel 
+# CDNBaseUrl = http://officecdn.microsoft.com/pr/7ffbc6bf-bc32-4f92-8982-f9dd17fd3114
+
+# Semi-Annual Enterprise Channel (Preview)
+# CDNBaseUrl = http://officecdn.microsoft.com/pr/b8f9b850-328d-4355-9145-c59439a0c4cf
+
+# Beta Channel
+# CDNBaseUrl = http://officecdn.microsoft.com/pr/5440fd1f-7ecb-4221-8110-145efaa6372f
+
 
 $baseUrl = "http://officecdn.microsoft.com/pr/492350f6-3a01-4f97-b9c0-c7c6ddf67d60"
 $regPath = "HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration\"
 $regKey = "CDNBaseUrl"
 $updatesPath = "HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration\"
 $officeUpdatePath = "HKLM:\\SOFTWARE\Policies\Microsoft\Office\16.0\Common\OfficeUpdate\"
+$regKey2 = "UpdateChannel"
 
 if (Test-Path $regPath)
 {
@@ -24,6 +43,22 @@ if (Test-Path $regPath)
     {
         New-ItemProperty -Name $regKey -Path $regPath -PropertyType string -Value $baseUrl -Force
     }
+    else 
+    {
+        Set-ItemProperty -Name $regKey -Path $regPath -Value $baseUrl -Force
+    }
+
+    $UpdateChannel = Get-ItemProperty $regPath -Name $regKey2 -ErrorAction SilentlyContinue
+
+    if (! $UpdateChannel)
+    {
+        New-ItemProperty -Name $regKey2 -Path $regPath -PropertyType string -Value $baseUrl -Force
+    }
+    else 
+    {
+        Set-ItemProperty -Name $regKey2 -Path $regPath -Value $baseUrl -Force
+    }
+
 
     if (Get-ItemProperty -Path $regPath -Name "UpdateUrl" -ErrorAction SilentlyContinue)
     {
