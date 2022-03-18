718f19cf5af9106b9dca9e8f72272dbfc32abd0f
Ian Daniels <ian.daniels@dxc.com>
2022-03-18 15:20:08 +0000

    Updated oh-my-posh

--- Powershell/Microsoft.PowerShell_profile.ps1
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -5,9 +5,10 @@ if ($host.Name -eq 'ConsoleHost')
 {
     Import-Module posh-git
     $env:POSH_GIT_ENABLED = $true
-    Import-Module oh-my-posh
+    #Import-Module oh-my-posh
     #Set-PoshPrompt -Theme Paradox
-    Set-PoshPrompt -Theme  ~/.oh-my-posh.omp.json
+    #Set-PoshPrompt -Theme  ~/.oh-my-posh.omp.json
+    oh-my-posh prompt init pwsh --config ~/.oh-my-posh.omp.json | Invoke-Expression
 
     Import-Module PSReadLine
     Set-PSReadLineOption -EditMode Emacs
